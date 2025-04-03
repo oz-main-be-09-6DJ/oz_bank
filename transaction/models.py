99060352a0c16cf2a3646c30ef058bfc7476fe8d
socical <socical.dev@gmail.com>
2025-04-03 17:12:08 +0900

    ✅ Test: CustomUser 관련하여 테스트 코드 작성
    유저를 생성하여 값이 제대로 저장되었는지 이메일로 유저 조회 되는지, deleted_at 에 정상적으로 반영되는지, admin인지 user 회원인지 확인 및 authentication 정상적으로 완료가 되는지 확인

--- transaction/models.py
+++ transaction/models.py
@@ -11,8 +11,5 @@ class Transaction(models.Model):
     transaction_method = models.CharField(null=False, choices=TRANSACTION_METHOD)
     created_date = models.DateTimeField(auto_now_add=True)
 
-    # TODO : 병합 후 주석 제거
-    # account = models.ForeignKey(Account, on_delete=models.CASCADE, related_name='transactions')
-
     def __str__(self):
         return f"{self.get_type_display()} - {self.transaction_amount}원"
