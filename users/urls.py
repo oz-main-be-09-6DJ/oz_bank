d217d89c968806de397ed59fd60d2016f87ab30d
uuuj0821 <uuuj0821@naver.com>
2025-04-04 07:50:23 +0900

    ✨ Feat: oauth naver
    # Conflicts:
    #	config/settings/base.py
    #	users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from users.views import *
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from . import oauth_views
 
 app_name='users_noti_api'
 
@@ -11,4 +12,9 @@ urlpatterns = [
     path("users/profile/",ProfileView.as_view(),name="user_profile"),
     path('users/logout/', LogoutView.as_view(), name='user_logout'),#api/users/logout
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),#api/token/refresh POST
+
+    # oauth naver
+    path('naver/login-page/', oauth_views.naver_login_page, name='naver_login_page'),  # 로그인 버튼 있는 페이지
+    path('naver/login/', oauth_views.NaverLoginRedirectView.as_view(), name='naver_login'),
+    path('naver/callback/', oauth_views.naver_callback, name='naver_callback'),
 ]
