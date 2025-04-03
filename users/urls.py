bbe7efac080e5b004369272ba28a6865118a9fe0
socical <socical.dev@gmail.com>
2025-04-04 06:44:45 +0900

    📝 Docs:users/urls.py 주석 작성

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 app_name='users_noti_api'
 
 urlpatterns = [
-    path("users/auth/verify-email/<str:token>/", VerifyEmailView.as_view(), name="verify-email"),
+    path("auth/verify-email/<str:token>/", VerifyEmailView.as_view(), name="verify-email"),
     path("users/signup/",UserSignUpAPIView.as_view(),name="user_signup"),
     path("users/login/",TokenObtainPairView.as_view(),name="user_login"),
     path("users/profile/",ProfileView.as_view(),name="user_profile"),
