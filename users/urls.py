7255b3d6a7097e4bd3ac905396eba9d2523a598c
GwonDohyeon <125738341+GwonDohyeon@users.noreply.github.com>
2025-04-04 05:54:08 +0900

    logout jwt blacklist

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from users.views import *
-from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
 
 app_name='users_noti_api'
 
@@ -9,4 +9,6 @@ urlpatterns = [
     path("users/signup/",UserSignUpAPIView.as_view(),name="user_signup"),
     path("users/login/",TokenObtainPairView.as_view(),name="user_login"),
     path("users/profile/",UserMeAPIView.as_view(),name="user_profile"),
+    path('users/logout/', LogoutView.as_view(), name='user_logout'),#api/users/logout
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),#api/token/refresh POST
 ]
