706258f0d94b2aeb60af241dab8633d3685b7e07
socical <socical.dev@gmail.com>
2025-04-04 05:56:02 +0900

    ✨ Feat: 회원정보 확인 및 수정, 유저 삭제 기능 구현
    # Conflicts:
    #	users/urls.py
    #	users/views.py

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from users.views import *
-from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 app_name='users_noti_api'
 
@@ -8,7 +8,7 @@ urlpatterns = [
     path("users/auth/verify-email/<str:token>/", VerifyEmailView.as_view(), name="verify-email"),
     path("users/signup/",UserSignUpAPIView.as_view(),name="user_signup"),
     path("users/login/",TokenObtainPairView.as_view(),name="user_login"),
-    path("users/profile/",UserMeAPIView.as_view(),name="user_profile"),
+    path("users/profile/",ProfileView.as_view(),name="user_profile"),
     path('users/logout/', LogoutView.as_view(), name='user_logout'),#api/users/logout
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),#api/token/refresh POST
 ]
--- users/views.py
+++ users/views.py
@@ -1,20 +1,23 @@
-from rest_framework.generics import CreateAPIView,RetrieveUpdateAPIView
-from users.serializers import UserSignUpSerializer,UserUpdateMeSerializer,UserReadMeSerializer
-from utils.email import send_verification_email
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 from rest_framework import generics
+from rest_framework import status
+from rest_framework.generics import CreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
-from rest_framework import status
-from django.contrib.auth import get_user_model
-from users.models import EmailVerificationToken
 from rest_framework_simplejwt.tokens import RefreshToken
+
+from users.models import EmailVerificationToken
+from users.serializers import UserSignUpSerializer, UserUpdateMeSerializer, UserReadMeSerializer
+from utils.email import send_verification_email
 
 CustomUser = get_user_model()
 
+
 class UserSignUpAPIView(CreateAPIView):
-    serializer_class=UserSignUpSerializer
+    serializer_class = UserSignUpSerializer
+
     def create(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
@@ -29,17 +32,26 @@ class UserSignUpAPIView(CreateAPIView):
         )
 
 
-# GET: 회원 정보 확인 / PATCH: 회원 정보 수정
-class UserMeAPIView(RetrieveUpdateAPIView):
-    permission_classes=[IsAuthenticated]
-    authentication_classes=[JWTAuthentication]
+# 유저프로필 조회, 수정, 삭제 뷰
+class ProfileView(RetrieveUpdateDestroyAPIView):
+    permission_classes = [IsAuthenticated]  # 로그인한 사용자만 접근
+
     def get_object(self):
         return self.request.user
+
     def get_serializer_class(self):
-        if self.request.method=='GET':
-            return UserReadMeSerializer
-        elif self.request.method=='PATCH':
-            return UserUpdateMeSerializer
+        if self.request.method == 'PATCH':
+            return UserUpdateMeSerializer  # 유저 프로필 수정
+        return UserReadMeSerializer  # 유저 프로필 조회
+
+    # 유저 프로필 삭제 (삭제일자 저장, 비활성화 상태?)
+    def delete(self, request, *args, **kwargs):
+        user = self.get_object()
+        user.deleted_at = timezone.now()
+        user.save(update_fields=['deleted_at'])
+
+        return Response({"message": "Delete successfully"}, status=status.HTTP_204_NO_CONTENT)
+
 
 # 이메일 인증 완료 처리
 class VerifyEmailView(generics.GenericAPIView):
@@ -57,7 +69,8 @@ class VerifyEmailView(generics.GenericAPIView):
 
         except EmailVerificationToken.DoesNotExist:
             return Response({"error": "유효하지 않은 토큰입니다."}, status=status.HTTP_400_BAD_REQUEST)
-        
+
+
 class LogoutView(generics.GenericAPIView):
     permission_classes = [IsAuthenticated]
 
