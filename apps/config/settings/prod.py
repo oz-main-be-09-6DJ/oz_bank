0979c82b6a23883b57f145eef493282ec6d18cf4
uuuj0821 <uuuj0821@naver.com>
2025-04-04 17:10:05 +0900

    ✨ Feat: rds setting

--- apps/config/settings/prod.py
+++ apps/config/settings/prod.py
@@ -6,3 +6,15 @@ DEBUG = os.getenv("DEBUG", default=False)
 ALLOWED_HOSTS = []
 
 ROOT_URLCONF = 'apps.config.urls.urls_prod'
+
+# DB연결 정보
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+    }
+}
