08ba1ff77e6c216dc7f31c91f2cdd65076f1dc47
GwonDohyeon <125738341+GwonDohyeon@users.noreply.github.com>
2025-04-04 15:01:13 +0900

    ACCOUNT REAL FINAL

--- account/serializers.py
+++ account/serializers.py
@@ -1,10 +1,10 @@
 from rest_framework import serializers
 from account.models import Account
 from users.serializers import UserReadMeSerializer
-
+from decimal import Decimal
 class AccountCreateSerializer(serializers.ModelSerializer):
     user=UserReadMeSerializer(read_only=True)#계정 주인 임의 변경 불가
-    balance = serializers.DecimalField(max_digits=18, decimal_places=2, read_only=True)
+    balance = serializers.DecimalField(max_digits=18, decimal_places=2, required=False)
     class Meta:
         model=Account
         fields=[ 'account_number','bank_code','account_type','balance','user']
@@ -13,7 +13,9 @@ class AccountCreateSerializer(serializers.ModelSerializer):
         if not request or not request.user.is_authenticated:
             raise serializers.ValidationError("로그인이 필요합니다.")
         validated_data['user'] = request.user
-        return super().create(validated_data)
+        balance = validated_data.pop('balance', Decimal("0.00"))  # balance가 없으면 기본값 0.00
+        account = Account.objects.create(balance=balance, **validated_data)  # balance 저장
+        return account
     def to_representation(self, instance):#Response의 user 필드에서 name만 포함하도록 수정
         data = super().to_representation(instance)  # 기존 직렬화된 데이터 가져오기
         if instance.user:  # user가 존재하는 경우
