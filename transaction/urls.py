5d1deba98b1617ec1e163ab57f4c428cd12ca661
socical <socical.dev@gmail.com>
2025-04-04 14:47:32 +0900

    💡 transaction urls 경로 수정

--- transaction/urls.py
+++ transaction/urls.py
@@ -5,9 +5,9 @@ app_name = 'transaction_api'
 
 urlpatterns = [
     # 거래 내역 리스트 조회 및 생성 API
-    path('transactions/', TransactionListCreateAPIView.as_view(), name='transaction-list-create'),
+    path('', TransactionListCreateAPIView.as_view(), name='transaction-list-create'),
 
     # 거래 내역 조회, 수정, 삭제 API (PK로 특정 거래를 조회)
-    path('transactions/detail/<int:pk>/', TransactionRetrieveUpdateDestroyAPIView.as_view(),
+    path('detail/<int:pk>/', TransactionRetrieveUpdateDestroyAPIView.as_view(),
          name='transaction-retrieve-update-destroy'),
 ]
