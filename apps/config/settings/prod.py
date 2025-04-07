94507dc26cd53fde05dcdccf136ba9f71474a695
Ubuntu <ubuntu@ip-172-31-35-12.ap-northeast-2.compute.internal>
2025-04-07 10:11:12 +0000

    🐛 Fix: Mail SMTP 500 error ISSUE 처리

--- apps/config/settings/prod.py
+++ apps/config/settings/prod.py
@@ -3,7 +3,7 @@ from .base import *
 # DEBUG 설정 (개발 환경에서는 보통 True로 설정)
 DEBUG = os.getenv("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "").split(",")
 
 ROOT_URLCONF = 'apps.config.urls.urls_prod'
 
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 set -e
-python manage.py migrate # 실행 전에 migrate 자동실행
+
+# 설정을 명확히 설정
+export DJANGO_SETTINGS_MODULE=config.settings.prod
+
+# manage.py 위치로 이동
+cd /oz_bank/apps
+
+# 마이그레이션 수행
+python manage.py migrate
+
+# 서버 실행
 gunicorn --bind 0.0.0.0:8000 config.wsgi:application --workers 2
