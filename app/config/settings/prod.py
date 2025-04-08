5bfd9600596ce1ab68ff4b7609aac29e28199cf0
GwonDohyeon <125738341+GwonDohyeon@users.noreply.github.com>
2025-04-08 15:52:49 +0900

    prod

--- app/config/settings/prod.py
+++ app/config/settings/prod.py
@@ -2,7 +2,7 @@ from .base import *
 import certifi
 # DEBUG 설정 (개발 환경에서는 보통 True로 설정)
 DEBUG = os.getenv("DEBUG", default=False)
-os.environ['SSL_CERT_FILE'] = certifi.where() 
+#os.environ['SSL_CERT_FILE'] = certifi.where() 
 ALLOWED_HOSTS = ['127.0.0.1']
 
 ROOT_URLCONF = 'config.urls.urls_prod'
@@ -20,7 +20,7 @@ DATABASES = {
 # Email
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.naver.com"
-EMAIL_USE_TLS = False    # 보안 연결(TLS)용으로 EMAIL_PORT 연결과 관련
+EMAIL_USE_TLS = True    # 보안 연결(TLS)용으로 EMAIL_PORT 연결과 관련
 EMAIL_PORT = 587
 EMAIL_HOST_USER = os.getenv("USER_ID")
 EMAIL_HOST_PASSWORD = os.getenv("PASSWORD")
